ee43aebc2c5293618efe6f5c848793a9254234fa
Sajid <Sajid@DESKTOP-OTIV9MA>
2020-03-25 00:15:16 +0530

    first commit

---
+++ src/com/git/Test1.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="yes";
+		if(hungry=="yes")
+		System.out.println("Need food");
+		else {
+			System.out.println("Do work");
+		}
+		
+
+	}
+
+}
